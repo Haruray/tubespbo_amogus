6437ae6fd9a14d10212942aa8a64e11e0246ab62
PlumStream24 <sigid.iqbal123@gmail.com>
2021-03-24 21:45:25 +0700

    some player commands. will continue later

--- main.cpp
+++ main.cpp
@@ -3,9 +3,24 @@
 #include "SupplementaryFunctions.h"
 using namespace std;
 
+//Help Command
+void Help() {
+    cout << "Player Commands :" << endl << endl;
+    cout << "o Move (W,A,S,D)" << endl;
+    cout << "o ShowEngimon" << endl;
+    cout << "o ShowEngimonStat" << endl;
+    cout << "o SwapEngimon" << endl;
+    cout << "o ShowSkillItem" << endl;
+    cout << "o UseSkillItem" << endl;
+    cout << "o Breed" << endl;
+    cout << "o Battle" << endl;
+    cout << "o q" << endl;
+}
+
 int main(){
+    string input;
     string name;
-    int input;
+    int starterEngimon;
     //PEMBUATAN PETA
     vector<Cell> vsea;
     vector<Cell> vgrass;
@@ -25,20 +40,20 @@ int main(){
     cout<<"1. "<<JackFrost.getName()<<endl;
     cout<<"2. "<<Dababy.getName()<<endl;
     cout<<"3. "<<Waluigi.getName()<<endl;
-    cin>>input;
-    while(input<=0 || input>3){
-        cout<<"Input not valid.\n"<<endl;
+    cin>>starterEngimon;
+    while(starterEngimon<=0 || starterEngimon>3){
+        cout<<"starterEngimon not valid.\n"<<endl;
         cout<<"Choose engimon (by number) :"<<endl;
         cout<<"1. "<<JackFrost.getName()<<endl;
         cout<<"2. "<<Dababy.getName()<<endl;
         cout<<"3. "<<Waluigi.getName()<<endl;
     }
-    if (input==1){
+    if (starterEngimon==1){
         cout<<"You've chosen Jack Frost!"<<endl;
         JackFrost.printDetail();
         ie.addItem(&JackFrost);
     }
-    else if (input==2){
+    else if (starterEngimon==2){
         cout<<"You've chosen Dababy! Less goo"<<endl;
         Dababy.printDetail();
         ie.addItem(&Dababy);
@@ -53,4 +68,76 @@ int main(){
     system("PAUSE");
     system("CLS");
     map.printMap(&p);
+
+    cout << "Let us commence forth!" << endl;
+    cout << "To show available commands, Type : 'Help!'" << endl;
+
+    while (1) {
+        cout << "Command: ";
+        cin >> input;
+
+        if (input == "w" || input == "a" || input == "s" || input == "d") {
+            if (input == "w") {
+                p.moveUp();
+            } else if (input == "a") {
+                p.moveLeft();
+            } else if (input == "s") {
+                p.moveDown();
+            } else if (input == "d") {
+                p.moveRight();
+            }
+            map.printMap(&p);
+
+        }
+        else if (input == "Help!") {
+            Help();
+
+        } else if (input == "ShowEngimon") {
+            p.showEngimonList();
+
+        } else if (input == "ShowEngimonStat") {
+            p.showEngimonData(*p.getActiveEngimon());
+
+        } else if (input == "SwapEngimon") {
+            p.swapActiveEngimon();
+
+        } else if (input == "ShowSkillItem") {
+            p.showSkillList();
+
+        } else if (input == "UseSkillItem") {
+            int skillIdx, engimonIdx;
+            p.showSkillList();
+            cout << "Choose which skill item to use : ";
+            do {
+                cin >> skillIdx;
+            } while (skillIdx < 1 || skillIdx > p.getInventorySkill()->getSize());
+            do {
+                cin >> engimonIdx;
+            } while (engimonIdx < 1 || engimonIdx > p.getInventoryEngimon()->getSize());
+            p.useSkillItem(p.getInventorySkill()->getItemById(skillIdx-1), p.getInventoryEngimon()->getItemById(engimonIdx-1));
+
+        } else if (input == "Breed") {
+            int eng1, eng2;
+            do {
+                do {
+                    cin >> eng1;
+                } while (eng1 < 1 || eng1 > p.getInventoryEngimon()->getSize());
+                do {
+                    cin >> eng2;
+                } while (eng2 < 1 || eng2 > p.getInventoryEngimon()->getSize());
+            } while (eng1 == eng2);
+            p.breeding(p.getInventoryEngimon()->getItemById(eng1-1), p.getInventoryEngimon()->getItemById(eng2-1));
+
+        } else if (input == "Battle") {
+            // insert battle
+
+        } else if (input == "q") {
+            cout << "Quitting . . . " << endl;
+            break;
+
+        } else {
+            cout << "Invalid command!" << endl;
+        }
+    }
+
 }
