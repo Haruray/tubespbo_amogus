78d7e6c1d430240f0acd607b15ac3074a15598f4
PlumStream24 <sigid.iqbal123@gmail.com>
2021-03-24 12:23:50 +0700

    Added more species and engimon

--- EngimonUniverse.h
+++ EngimonUniverse.h
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include "Player.h"
+#include "Engimon.h"
 #include "Inventory.h"
 #include "Map.h"
 using namespace std;
@@ -18,11 +19,24 @@ Element Ground("Ground");
 Element Ice("Ice");
 
 //Species
+Species Watortle("Watortle", "Waaaa!", Skill("Waterfall", 100, 5, vector<Element> {Water}));
 Species Koikingu("Koikingu", "I'm a fish, meow", Skill("WaterSplash", 100, 5, vector<Element> {Water}));
-Species Impostor("Impostor", "Sus!", Skill("Sabotage", 100, 5, vector<Element> {Ice}));
-Species Bufumon("Bufumon", "", Skill("Bufu", 100, 5, vector<Element> {Ice}));
-Species Rizadon("Rizadon", "", Skill("FireFang", 100, 5, vector<Element> {Fire}));
-Species Diguda("Digoda", "Toransu!", Skill("SandAttack", 100, 5, vector<Element> {Ground}));
+Species Impostor("Impostor", "Sus!", Skill("Sabotage", 100, 5, vector<Element> {Electric}));
+Species Raishuu("Raishuu", "Rokubyou", Skill("Mandom", 100, 5, vector<Element> {Electric}));
+Species Saider("Saider", "Apple Sauce", Skill("Crimson", 100, 5, vector<Element> {Fire}));
+Species Rizadon("Rizadon", "420 Moyase!", Skill("FireFang", 100, 5, vector<Element> {Fire}));
+Species Bufumon("Bufumon", "Hee Ho!", Skill("Bufu", 100, 5, vector<Element> {Ice}));
+Species IceCube("IceCube", "I'm just an ice cube", Skill("White Album", 100, 5, vector<Element> {Ice}));
+Species Golem("Golem", "I'm the gaming golem", Skill("Rock", 100, 5, vector<Element> {Ground}));
+Species Diguda("Diguda", "Toransu!", Skill("SandAttack", 100, 5, vector<Element> {Ground}));
+
+//Engimon
+vector<Skill> skills;
+Engimon JackFrost("Jack Frost", nullptr, nullptr, Bufumon, vector<Skill>, vector<Element> {Ice}, 1, 10000);
+Engimon Raool("Raool", nullptr, nullptr, Saider, vector<Skill>, vector<Element> {Fire}, 1, 10000);
+Engimon Dababy("Dababy", nullptr, nullptr, Golem, vector<Skill>, vector<Element> {Ground}, 1, 10000);
+Engimon Waluigi("Waluigi", nullptr, nullptr, Watortle, vector<Skill>, vector<Element> {Water}, 1, 10000);
+Engimon Ringo("Ringo", nullptr, nullptr, Raishuu, vector<Skill>, vector<Element> {Electric}, 1, 10000);
 
 
 #endif
