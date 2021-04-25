44a0b9f995c367344c540b37653f92a01f6a49f0
SafiqF <63040763+Haruray@users.noreply.github.com>
2021-04-25 12:21:32 +0700

    methodsnya dijadiin static

--- willywangky/src/willywank/mainobjects/SupplementaryFunctions.java
+++ willywangky/src/willywank/mainobjects/SupplementaryFunctions.java
@@ -3,7 +3,7 @@ package willywank.mainobjects;
 import java.util.List;
 
 public class SupplementaryFunctions {
-    public float getHighestAdvantage(List<Element> le1, List<Element> le2){
+    public static float getHighestAdvantage(List<Element> le1, List<Element> le2){
         float max = 0;
         for (int i = 0; i < le1.size() ; i++){
             for (int j = 0 ; j < le2.size() ; j++){
@@ -15,7 +15,7 @@ public class SupplementaryFunctions {
         return max;
     }
 
-    public float sumSkillLevelAndPower(List<Skill> ls){
+    public static float sumSkillLevelAndPower(List<Skill> ls){
         int sum = 0;
         for (int i = 0 ; i < ls.size() ; i++){
             sum += ls.get(i).getBasePower() * ls.get(i).getMasteryLevel();
@@ -23,13 +23,13 @@ public class SupplementaryFunctions {
         return (float) sum;
     }
 
-    public boolean battle(Engimon ourEngimon, Enemy enemyEngimon){
+    public static boolean battle(Engimon ourEngimon, Enemy enemyEngimon){
         float ourPower = ourEngimon.getLevel() * getHighestAdvantage(ourEngimon.getElements() , enemyEngimon.getElements()) + sumSkillLevelAndPower(ourEngimon.getSkills());
         float enemyPower = enemyEngimon.getLevel() * getHighestAdvantage(enemyEngimon.getElements() , ourEngimon.getElements()) + sumSkillLevelAndPower(enemyEngimon.getSkills());
         return ourPower >= enemyPower;
     }
 
-    void playerMove(Player p, Map map, String input){
+    public static void playerMove(Player p, Map map, String input){
         if (input.equals("w")){
             if (p.getPosX()-1 >= 0){
                 if (!map.getCell(p.getPosX()-1, p.getPosY()).isOccupied()){ //tidak boleh tabrakan dengan enemy lain
